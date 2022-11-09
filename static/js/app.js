a3b2d233dc0b5ea46c9690591a7abc12d2f6a832
ldvermunt <ldvermunt@outlook.com>
2022-11-09 09:55:51 -0500

    Commit UFO Code

--- static/js/app.js
+++ static/js/app.js
@@ -22,3 +22,29 @@ function buildTable(data) {
       );
     });
 }
+
+//Write a function to make a filter for the date in the data
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  };
+
+  // make a button to handle the date filter coded above
+  d3.selectAll("#filter-btn").on("click", handleClick);
+
+  // Build the table when the page loads.  The raw data table so the user can see what is there.
+buildTable(tableData);
